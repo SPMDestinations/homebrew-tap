0a0ca6a55ea5dad5e8e1ee5924e4fd90c8c897ca
Helge Heß <devteam@zeezide.de>
2020-08-03 14:16:51 +0200

    Add host LLD Formula
    ... avoid having to download all of clang, swift is bad enough.

--- clang-llvm-bin-8.rb
+++ clang-llvm-bin-8.rb
@@ -6,6 +6,8 @@ class ClangLlvmBin8 < Formula
   # curl -L https://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-apple-darwin.tar.xz | shasum -a 256
   sha256 "94ebeb70f17b6384e052c47fef24a6d70d3d949ab27b6c83d4ab7b298278ad6f"
 
+  conflicts_with "host-lld-bin-8", :because => "host-lld-bin-8 also ships `lld`"
+
   # This patch is for adding the Makefile which does the installation
   patch do
     url "https://helgehess.eu/patches/clang-llvm-8.0.0-makefile.patch"
