0bcb56c55417abeeba36c48582fd164118b164de
Helge Heß <devteam@zeezide.de>
2020-08-16 16:57:18 +0200

    Use swift-lambda 0.1.1
    ... w/ the fixed --static.

--- swift-lambda.rb
+++ swift-lambda.rb
@@ -2,13 +2,13 @@ class SwiftLambda < Formula
 
   desc "Scripts to build and deploy Swift Package Manager projects on AWS Lambda"
   homepage "https://github.com/SwiftXcode/swift-lambda"
-  url "https://github.com/SwiftXcode/swift-lambda/archive/0.1.0.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-lambda/archive/0.1.0.tar.gz | shasum -a 256
-  sha256 "da340e28adf1a4e8b8123c3e1faf66ab23bab8587ab13db4c518334d069677eb"
+  url "https://github.com/SwiftXcode/swift-lambda/archive/0.1.1.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-lambda/archive/0.1.1.tar.gz | shasum -a 256
+  sha256 "0409d93134ed595223c7f09d61bd7085d60f42e8b5e02b19c5ceb08f8d0181ec"
 
-  version "0.1.0"
+  version "0.1.1"
   version_scheme 1
-  revision 2
+  revision 1
   
   depends_on "spmdestinations/tap/spm-dest-5.3-x86_64-amazonlinux2"
   depends_on "spmdestinations/tap/spm-dest-5.2-x86_64-amazonlinux2" => [:optional]
