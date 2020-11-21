ce05ab854d3eab4a133a32054bb8b967ff9cf1bb
Helge Heß <devteam@zeezide.de>
2020-11-21 16:09:31 +0100

    Update host toolchain to 5.3.1
    ...

--- swift-xctoolchain-5.3.rb
+++ swift-xctoolchain-5.3.rb
@@ -1,21 +1,21 @@
 class SwiftXctoolchain53 < Formula
   
-  desc     "A macOS Swift 5.3 DEV SNAPSHOT toolchain for cross compilation purposes"
+  desc     "A macOS Swift 5.3.1 SNAPSHOT toolchain for cross compilation purposes"
   homepage "https://swift.org"
-  url      "https://swift.org/builds/swift-5.3-branch/xcode/swift-5.3-DEVELOPMENT-SNAPSHOT-2020-08-15-a/swift-5.3-DEVELOPMENT-SNAPSHOT-2020-08-15-a-osx.pkg"
-  # curl -L https://swift.org/builds/swift-5.3-branch/xcode/swift-5.3-DEVELOPMENT-SNAPSHOT-2020-08-15-a/swift-5.3-DEVELOPMENT-SNAPSHOT-2020-08-15-a-osx.pkg | shasum -a 256
-  sha256   "c283e5f50a171bdfbde87480c0c996ec97a137a6885c68c51d040af0e424b282"
+  url      "https://swift.org/builds/swift-5.3.1-release/xcode/swift-5.3.1-RELEASE/swift-5.3.1-RELEASE-osx.pkg"
+  # curl -L https://swift.org/builds/swift-5.3.1-release/xcode/swift-5.3.1-RELEASE/swift-5.3.1-RELEASE-osx.pkg | shasum -a 256
+  sha256   "9df54d4f5b866845751775cd7836850224b443dc055d0503fb01128775688b44"
   
-  version "swift-5.3-DEVELOPMENT-SNAPSHOT-2020-08-15-a"
-  version_scheme 1
-  revision 2
+  version "swift-5.3.1"
+  version_scheme 2
+  revision 3
   
   def install
     system "xar -xf *.pkg"
     system "mkdir", "swift.xctoolchain"
     system "cd swift.xctoolchain && cat ../*.pkg/Payload | gunzip -dc | cpio -i"
-    system "mkdir", "-p", "#{prefix}/lib/swift/xctoolchains/x86_64-apple-darwin/5.3-dev-2020-08-15-a"
-    system "cp -ac swift.xctoolchain #{prefix}/lib/swift/xctoolchains/x86_64-apple-darwin/5.3-dev-2020-08-15-a/swift.xctoolchain"
-    system "ln", "-sf", "#{prefix}/lib/swift/xctoolchains/x86_64-apple-darwin/5.3-dev-2020-08-15-a", "#{prefix}/lib/swift/xctoolchains/x86_64-apple-darwin/5.3-current"
+    system "mkdir", "-p", "#{prefix}/lib/swift/xctoolchains/x86_64-apple-darwin/5.3.1"
+    system "cp -ac swift.xctoolchain #{prefix}/lib/swift/xctoolchains/x86_64-apple-darwin/5.3.1/swift.xctoolchain"
+    system "ln", "-sf", "#{prefix}/lib/swift/xctoolchains/x86_64-apple-darwin/5.3.1", "#{prefix}/lib/swift/xctoolchains/x86_64-apple-darwin/5.3-current"
   end
 end
