9d6e66892e0af4af96e33cb3a2965658189e3fcc
Helge Heß <devteam@zeezide.de>
2021-02-03 13:42:12 +0100

    Minor bugfix in swift-lambda
    ...

--- swift-lambda.rb
+++ swift-lambda.rb
@@ -2,13 +2,13 @@ class SwiftLambda < Formula
 
   desc "Scripts to build and deploy Swift Package Manager projects on AWS Lambda"
   homepage "https://github.com/SwiftXcode/swift-lambda"
-  url "https://github.com/SwiftXcode/swift-lambda/archive/0.1.2.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-lambda/archive/0.1.2.tar.gz | shasum -a 256
-  sha256 "014e1cad25dfeabf9189470b1dc000d1af431c5ef4317c4703105995b121d44f"
+  url "https://github.com/SwiftXcode/swift-lambda/archive/0.1.3.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-lambda/archive/0.1.3.tar.gz | shasum -a 256
+  sha256 "a0a6255b8254e59b62bc903f06ae472200a686eb38db5a6448038a11ac40ec7e"
 
-  version "0.1.2"
+  version "0.1.3"
   version_scheme 1
-  revision 2
+  revision 3
   
   depends_on "spmdestinations/tap/spm-dest-5.3-x86_64-amazonlinux2"
   depends_on "spmdestinations/tap/spm-dest-5.2-x86_64-amazonlinux2" => [:optional]
