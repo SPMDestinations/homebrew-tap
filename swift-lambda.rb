a6fdf77d2eb985e7ccb67ccfb65949014aff0ab5
Helge Heß <devteam@zeezide.de>
2021-02-03 13:37:27 +0100

    Update swift-lambda to 0.1.2
    This comes w/ support for SPM resources.

--- swift-lambda.rb
+++ swift-lambda.rb
@@ -2,13 +2,13 @@ class SwiftLambda < Formula
 
   desc "Scripts to build and deploy Swift Package Manager projects on AWS Lambda"
   homepage "https://github.com/SwiftXcode/swift-lambda"
-  url "https://github.com/SwiftXcode/swift-lambda/archive/0.1.1.tar.gz"
-  # curl -L https://github.com/SwiftXcode/swift-lambda/archive/0.1.1.tar.gz | shasum -a 256
-  sha256 "0409d93134ed595223c7f09d61bd7085d60f42e8b5e02b19c5ceb08f8d0181ec"
+  url "https://github.com/SwiftXcode/swift-lambda/archive/0.1.2.tar.gz"
+  # curl -L https://github.com/SwiftXcode/swift-lambda/archive/0.1.2.tar.gz | shasum -a 256
+  sha256 "014e1cad25dfeabf9189470b1dc000d1af431c5ef4317c4703105995b121d44f"
 
-  version "0.1.1"
+  version "0.1.2"
   version_scheme 1
-  revision 1
+  revision 2
   
   depends_on "spmdestinations/tap/spm-dest-5.3-x86_64-amazonlinux2"
   depends_on "spmdestinations/tap/spm-dest-5.2-x86_64-amazonlinux2" => [:optional]
